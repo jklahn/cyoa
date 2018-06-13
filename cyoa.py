bb746935d87d582456850ab5c5471acb897a6584
jklahn <jdkforchrist@gmail.com>
2018-06-12 22:43:45 -0400

    added validation of selection

--- cyoa.py
+++ cyoa.py
@@ -10,20 +10,52 @@ class Page(object):
         self.body = page["body"]
         self.prompt = page["prompt"]
         self.linked_pages = page["linked_pages"]
+        self.visit_counter = 0
+
+
+class Book(object):
+    def __init__(self, game_data):
+        self.pages = {}
+        self.current_page = None
+        self.selection_range = 0
+        self.selection = None
+
+        # add pages to the book
+        for page in game_data["pages"]:
+            self.pages[page["name"]] = Page(page)
+
+    def read(self):
+        self.nav_to_page("start")
+        self.read_current_page()
+        self.get_input_validate_selection()
+        print(self.selection)
+
+    def get_input_validate_selection(self):
+        self.selection_range = len(self.current_page.linked_pages)
+        self.selection = input("\nEnter a number (1 - "
+                          + str(self.selection_range) + "): ")
+        while int(self.selection) not in range(1, self.selection_range):
+            print("Invalid number")
+            self.selection = input("\nEnter a number (1 - "
+                              + str(self.selection_range) + "): ")
+
+    def nav_to_page(self, page_name):
+        self.current_page = self.pages[page_name]
+
+    def read_current_page(self):
+        print(self.current_page.body)
+        print(self.current_page.prompt)
+
+        for index, link in enumerate(self.current_page.linked_pages):
+            print(self.pages[link].title + " [" + str(index + 1) + "]")
 
 
 if __name__ == '__main__':
     with open(game_data_path) as f:
-        data = load(f)
+        game_data = load(f)
 
-    book = {}
-    for page in data["pages"]:
-        book[page["name"]] = Page(page)
+    book = Book(game_data)
 
-    current_page = book["start"]
+    book.read()
 
-    print(current_page.body)
-    print(current_page.prompt)
 
-    for index, link in enumerate(current_page.linked_pages):
-        print(link + " [" + str(index + 1) + "]")
