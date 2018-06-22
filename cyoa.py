7a1f36d15409d6ac274f20c573a261a2bea65f7f
Joshua Klahn <klahn@us.ibm.com>
2018-06-22 15:34:37 -0400

    Globalized the line length.  Added ability to remove from inventory.

--- cyoa.py
+++ cyoa.py
@@ -2,22 +2,13 @@ from json import load
 from os import listdir
 
 
-def get_data_file_path():
-    for file in listdir('data/'):
-        print(file)
-        if file.endswith('.json'):
-            return 'data/' + file
-
-    print("ERROR: Failed to find a '.json' game data file in the 'data' directory.")
-
-
 class Page(object):
     def __init__(self, page, max_line_length=80):
         self.max_line_length = max_line_length
         self.name = page["name"]
         self.title = page["title"]
-        self.body = self.wrap_text(page["body"])
-        self.prompt = self.wrap_text(page["prompt"])
+        self.body = wrap_text(page["body"], max_line_lenth)
+        self.prompt = wrap_text(page["prompt"], max_line_lenth)
         self.short_description = page["short_description"]
         self.linked_pages = page["linked_pages"]
         self.visit_counter = 0
@@ -26,25 +17,6 @@ class Page(object):
         self.remove_from_inventory = page.get("remove_from_inventory")
         self.check_in_inventory = page.get("check_in_inventory")
         self.inventory_alt_page = page.get("inventory_alt_page")
-
-    def wrap_text(self, text):
-        formatted_text = ""
-        for string in text.split("\n"):
-            if len(string) > self.max_line_length:  # if the string is greater than the max allowed per line
-                line = ""
-                for word in string.split():  # split string into words if greater than max
-                    if len(line) + len(word) > self.max_line_length:
-                        formatted_text += "\n" + line
-                        line = "" + word + " "
-                    else:
-                        line += word + " "
-
-                formatted_text += "\n" + line
-            else:
-                string += "\n"  # add the new line back
-                formatted_text += string
-
-        return formatted_text
 
 
 class Book(object):
@@ -55,14 +27,16 @@ class Book(object):
         self.selection = None
         self.next_page = None
         self.prev_page = None
+        self.max_line_lenth = game_data["max_line_length"]
 
         # add pages to the book
         for page in game_data["pages"]:
-            self.pages[page["name"]] = Page(page)
+            self.pages[page["name"]] = Page(page, self.max_line_lenth)
 
         # set state
         self.start_health = game_data["starting_health"]
         self.health = self.start_health
+        self.welcome_message = wrap_text(game_data["welcome_message"], self.max_line_lenth)
         self.inventory = []
 
     def read(self):
@@ -106,6 +80,8 @@ class Book(object):
         self.current_page = self.pages[page_name]
 
     def read_current_page(self):
+        print("")
+        input("Press any key to continue...")
         self.print_title()
         if self.current_page.visit_counter > 0:
             print(self.current_page.short_description)
@@ -150,6 +126,8 @@ class Book(object):
             if self.health > 100:  # Can't let health go over 100
                 self.health = 100
 
+            print("Current Health: " + str(self.health))
+
         if self.health <= 0:  # Game Over if your health goes below 0
             print("Your health has dropped below 0.\n\nGAME OVER\n")
             input("Press Enter to restart...")
@@ -157,23 +135,54 @@ class Book(object):
 
     def check_inventory(self):
         if self.current_page.add_to_inventory:
-            self.inventory.append(self.current_page.add_to_inventory)  # add to inventory
-            # remove the link to inventory page from the pevious page so it can't be accessed
+            if self.current_page.add_to_inventory not in self.inventory:
+                self.inventory.append(self.current_page.add_to_inventory)  # add to inventory
+
+            # remove the link to inventory page from the previous page so it can't be accessed
             self.prev_page.linked_pages.remove(self.current_page.name)
+            print(self.current_page.body)
+            self.check_health()
             print("\nCurrent Inventory:")
             print(self.inventory)
-            print("\n")
             self.nav_to_page(self.prev_page.name)  # go back to the previous pag
 
-        if self.current_page.remove_from_inventory: # remove from inventory if present
-            if self.current_page.remove_from_inventory in self.inventory:
-                self.inventory.remove(self.current_page.remove_from_inventory)
+        if self.current_page.remove_from_inventory and self.current_page.remove_from_inventory in self.inventory:
+            self.inventory.remove(self.current_page.remove_from_inventory)  # remove from inventory if present
+            print(self.current_page.body)
+            print("\nCurrent Inventory:")
+            print(self.inventory)
+            self.nav_to_page(self.current_page.inventory_alt_page)
 
-        if self.current_page.check_in_inventory:  # use alt page links if present in inventory
-            print("Inventory check is good:")
-            print(self.current_page.check_in_inventory)
-            if self.current_page.check_in_inventory and self.current_page.check_in_inventory in self.inventory:
-                self.nav_to_page(self.current_page.inventory_alt_page)
+        if self.current_page.check_in_inventory and self.current_page.check_in_inventory in self.inventory:
+            self.nav_to_page(self.current_page.inventory_alt_page)   # use alt page links if present in inventory
+
+
+def get_data_file_path():
+    for file in listdir('data/'):
+        if file.endswith('.json'):
+            return 'data/' + file
+
+    print("ERROR: Failed to find a '.json' game data file in the 'data' directory.")
+
+
+def wrap_text(text, max_line_length):
+    formatted_text = ""
+    for string in text.split("\n"):
+        if len(string) > max_line_length:  # if the string is greater than the max allowed per line
+            line = ""
+            for word in string.split():  # split string into words if greater than max
+                if len(line) + len(word) > max_line_length:
+                    formatted_text += "\n" + line
+                    line = "" + word + " "
+                else:
+                    line += word + " "
+
+            formatted_text += "\n" + line
+        else:
+            string += "\n"  # add the new line back
+            formatted_text += string
+
+    return formatted_text
 
 
 if __name__ == '__main__':
@@ -181,7 +190,8 @@ if __name__ == '__main__':
     while True:
         with open(get_data_file_path()) as f:
             game_data = load(f)
-
+        max_line_lenth = game_data["max_line_length"]
         book = Book(game_data)
+        print(book.welcome_message)
         book.nav_to_page("start")
         book.read()
